ea8b5384e7df03bcf936ee8f4a4df12d22ae2554
Core-Ventura <core.ventura@gmail.com>
2020-01-21 15:34:54 +0000

    Finished
    Finished

---
+++ Assets/AIControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    [HideInInspector]
+    public NavMeshAgent agent;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        agent = this.GetComponent<NavMeshAgent>();
+        float nScale = Random.Range(0.25f, 0.85f);
+        transform.localScale = Vector3.one * nScale;
+
+        // The smaller the robot, the fastest it is
+        agent.speed = 5f * (1-nScale);
+        agent.acceleration = 8f * (1-nScale);
+        agent.angularSpeed = 200 * (1-nScale);
+
+        agent.height = nScale;
+        animator.speed *= (1-nScale);
+    }
+
+    
+}
---
+++ Assets/AgentManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+
+    public Transform playerController;        
+    GameObject[] agents;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject a in agents)
+        {
+            a.GetComponent<AIControl>().agent.SetDestination(playerController.position);
+        }
+    }
+
+}
